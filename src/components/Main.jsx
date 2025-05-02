f7c07d3a71677e9c490a27da1f353984a410c49b
Isabelle França <isabellefrancax@gmail.com>
2025-05-02 09:30:23 -0300

    search

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import Card from "./Card"
+import { useState } from "react";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export default function Main() {
+
+    const [search, setSearch] = useState('')
+
     const { isPending, error, data } = useQuery({
         queryKey: ['repoData'],
         queryFn: () =>
@@ -16,10 +20,17 @@ export default function Main() {
 
       if (error) return 'An error has occurred: ' + error.message
 
+    const filteredMovies = data.results.filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()))
+
     return (
-        <div className="min-h-screen flex items-center justify-center bg-black">
+        <div className="min-h-screen flex items-center justify-center bg-black flex-col">
+
+            <div className="mt-6">
+                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className="py-2 px-6 w-full border border-gray-400 rounded bg-transparent text-white shadow-md" />
+            </div>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-6">
-                {data.results.map((title) => (
+                {filteredMovies.map((title) => (
                     <Card key={title.id} title={title.title} poster={title.poster_path} />
                 ))}
             </div>
