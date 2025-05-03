a8ec15d0932ef6fe8a7d436d980eb9fa30b9e0ac
Isabelle França <isabellefrancax@gmail.com>
2025-05-03 07:36:30 -0300

    search

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,35 +5,36 @@ import { useState } from "react";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export default function Main() {
+  // search
+  const [search, setSearch] = useState('')
 
-    const [search, setSearch] = useState('')
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`).then((res) =>
+        res.json(),
+      ),
+  })
 
-    const { isPending, error, data } = useQuery({
-        queryKey: ['repoData'],
-        queryFn: () =>
-          fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`).then((res) =>
-            res.json(),
-          ),
-      })
+  if (isPending) return 'Loading...'
 
-      if (isPending) return 'Loading...'
+  if (error) return 'An error has occurred: ' + error.message
 
-      if (error) return 'An error has occurred: ' + error.message
+  // search
+  const filteredMovies = data.results.filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()))
 
-    const filteredMovies = data.results.filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()))
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black flex-col">
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-black flex-col">
+      <div className="mt-6">
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className="py-2 px-6 w-full border border-gray-400 rounded bg-transparent text-white shadow-md" />
+      </div>
 
-            <div className="mt-6">
-                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className="py-2 px-6 w-full border border-gray-400 rounded bg-transparent text-white shadow-md" />
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-6">
-                {filteredMovies.map((title) => (
-                    <Card key={title.id} title={title.title} poster={title.poster_path} />
-                ))}
-            </div>
-        </div>
-    )
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-6">
+        {filteredMovies.map((title) => (
+          <Card key={title.id} title={title.title} poster={title.poster_path} />
+        ))}
+      </div>
+    </div>
+  )
 }
